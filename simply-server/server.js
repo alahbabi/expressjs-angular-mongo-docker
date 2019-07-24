4df32ef3ebb36ea80eec46a0d8fcdaf28d8d070b
alahbabi <lahbabi.abdelkarim@gmail.com>
2019-07-24 20:42:34 +0200

    ALI : File config

--- simply-server/server.js
+++ simply-server/server.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require('cors')
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const config = require("config")
 
 var app = express();
-
+// Database connection
+mongoose.connect(config.get('mongodb.connectionString'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
 app.use(cors());
 
 // Starting application
