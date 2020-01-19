8d4b8439f67094106001f1bd16ed07e01ee5ac49
alahbabi <lahbabi.abdelkarim@gmail.com>
2020-01-19 14:24:33 +0100

    ALI : useHash

--- simply-angular/src/app/app.routing.ts
+++ simply-angular/src/app/app.routing.ts
@@ -31,4 +31,4 @@ const routes: Routes = [
     { path: '**', redirectTo: '' }
 ];
 
-export const appRoutingModule = RouterModule.forRoot(routes);
+export const appRoutingModule = RouterModule.forRoot(routes, { useHash: true });
